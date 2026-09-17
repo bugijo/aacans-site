a848ee1bd42de9ec361474f11925511bf5dcd765
bugijo <bugijo@users.noreply.github.com>
2026-09-17 06:52:02 -0300

    fix: compatibilizar sitemap com GitHub Pages

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,1 @@
-import type { MetadataRoute } from 'next'; export default function robots():MetadataRoute.Robots{return {rules:{userAgent:'*',allow:'/'},sitemap:'https://aacans.org.br/sitemap.xml'}}
+import type { MetadataRoute } from 'next'; export const dynamic='force-static'; export default function robots():MetadataRoute.Robots{return {rules:{userAgent:'*',allow:'/'},sitemap:'https://bugijo.github.io/aacans-site/sitemap.xml'}}
